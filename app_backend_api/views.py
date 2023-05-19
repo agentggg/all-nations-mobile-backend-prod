0a0351dbaaa4a348a5515559fce1537c00f63f8b
agentggg <47467793+agentggg@users.noreply.github.com>
2023-05-19 12:40:16 -0700

    pushing outreach bug

--- app_backend_api/views.py
+++ app_backend_api/views.py
@@ -373,7 +373,7 @@ def register_api_data(request):
     return Response("Please screenshot and contact your system admin if you are seeing this page reg-api")
 
 @api_view(['GET', 'POST'])
-def outreach_registration_api_data(request):   
+def outreach_registration_api(request):   
     username = request.data.get('username', False)
     #gathers the profile first name info to save in the DB
     interactionTime = request.data.get('time', False)
