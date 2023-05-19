c4377f83682b973f7049aaf0d2f31bac17f13c62
agentggg <47467793+agentggg@users.noreply.github.com>
2023-05-19 15:57:51 -0700

    fixing token

--- app_backend_api/views.py
+++ app_backend_api/views.py
@@ -375,6 +375,7 @@ def register_api_data(request):
 @api_view(['GET', 'POST'])
 def outreach_registration_api_text(request):   
     username = request.data.get('username', False)
+    print(f"==>> username: {username}")
     #gathers the profile first name info to save in the DB
     interactionTime = request.data.get('time', False)
     # retrieves time from frontend, time in user local timezone
